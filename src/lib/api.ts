bb560a3b26d3ced77db0714af403b18f1e59fa8a
ceedadev <ceedadev@gmail.com>
2023-11-26 14:19:32 +0700

    add api function

---
+++ src/lib/api.ts
@@ -0,0 +1,32 @@
+const API_URL = process.env.GRAPHQL_API_URL as string;
+
+export async function fetchAPI(
+  query = "",
+  { variables }: Record<string, any> = {}
+) {
+  const headers: any = { "Content-Type": "application/json" };
+
+  if (process.env.WORDPRESS_AUTH_REFRESH_TOKEN) {
+    headers[
+      "Authorization"
+    ] = `Bearer ${process.env.WORDPRESS_AUTH_REFRESH_TOKEN}`;
+  }
+
+  // WPGraphQL Plugin must be enabled
+
+  const res = await fetch(API_URL, {
+    headers,
+    method: "POST",
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  });
+
+  const json = await res.json();
+  if (json.errors) {
+    console.error(json.errors);
+    throw new Error("Failed to fetch error");
+  }
+  return json.data;
+}
