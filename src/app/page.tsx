aa6b4214671622eab4dacbb9a9912979c5a53a08
ceedadev <ceedadev@gmail.com>
2023-11-27 19:06:29 +0700

    testing data fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
-function Home() {
-  return <div>Home</div>;
-}
+import Link from "next/link";
+import Image from "next/image";
+import * as dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import utc from "dayjs/plugin/utc";
 
-export default Home;
+dayjs.extend(relativeTime);
+dayjs.extend(utc);
+
+import { getAllPostForHome } from "@/lib/api";
+import PostPreviewCard from "@/components/post-preview-card";
+
+export default async function Home() {
+  const data = await getAllPostForHome("");
+  return (
+    <div className="container mx-auto">
+      {/* <div>{JSON.stringify(data)}</div> */}
+      <div className="grid gap-4">
+        {data.edges.map(({ node }: any) => (
+          <PostPreviewCard node={node} />
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,6 @@
 const API_URL = process.env.GRAPHQL_API_URL as string;
 
-export async function fetchAPI(
-  query = "",
-  { variables }: Record<string, any> = {}
-) {
+async function fetchAPI(query = "", { variables }: Record<string, any> = {}) {
   const headers: any = { "Content-Type": "application/json" };
 
   if (process.env.WORDPRESS_AUTH_REFRESH_TOKEN) {
@@ -29,4 +26,46 @@ export async function fetchAPI(
     throw new Error("Failed to fetch error");
   }
   return json.data;
+}
+
+export async function getAllPostForHome(preview: any) {
+  const data = await fetchAPI(
+    `
+    query AllPosts {
+      posts(first: 20, where: { orderby: { field: DATE, order: DESC } }) {
+        edges {
+          node {
+            title
+            excerpt
+            slug
+            date
+            featuredImage {
+              node {
+                sourceUrl
+              }
+            }
+            author {
+              node {
+                name
+                firstName
+                lastName
+                avatar {
+                  url
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `,
+    {
+      variables: {
+        onlyEnabled: !preview,
+        preview,
+      },
+    }
+  );
+
+  return data?.posts;
 }
