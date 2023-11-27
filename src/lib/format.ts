73143c1b04b536bdf0a7db763e582ace291a4ebc
ceedadev <ceedadev@gmail.com>
2023-11-27 19:06:37 +0700

    added timeago

---
+++ src/lib/format.ts
@@ -0,0 +1,10 @@
+import * as dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import utc from "dayjs/plugin/utc";
+dayjs.extend(relativeTime);
+dayjs.extend(utc);
+
+export function formatTimeAgo(utcstring: string) {
+  const date = dayjs.utc(utcstring);
+  return date.fromNow();
+}
