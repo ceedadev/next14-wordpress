3d09c4b9c7a06f5cedeeba4d22c6dc7a64cf676b
ceedadev <ceedadev@gmail.com>
2023-11-28 16:39:11 +0700

    cleanup code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,16 @@
-import Link from "next/link";
-import Image from "next/image";
-import * as dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
-import utc from "dayjs/plugin/utc";
-
-dayjs.extend(relativeTime);
-dayjs.extend(utc);
-
 import { getAllPostForHome } from "@/lib/api";
 import PostPreviewCard from "@/components/post-preview-card";
 
 export default async function Home() {
   const data = await getAllPostForHome("");
   return (
-    <div className="container mx-auto">
-      {/* <div>{JSON.stringify(data)}</div> */}
+    <main className="container mx-auto">
+      <div>{JSON.stringify(data)}</div>
       <div className="grid gap-4">
         {data.edges.map(({ node }: any) => (
-          <PostPreviewCard node={node} />
+          <PostPreviewCard key={node.id} node={node} />
         ))}
       </div>
-    </div>
+    </main>
   );
 }
