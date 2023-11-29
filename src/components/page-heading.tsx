abd9ccd1ca9f3a6f1e0416b58d52b65c7e98400a
ceedadev <ceedadev@gmail.com>
2023-11-29 10:24:53 +0700

    new components

---
+++ src/components/page-heading.tsx
@@ -0,0 +1,5 @@
+export const PageHeading = ({ text }: { text: string }) => (
+  <p className="uppercase tracking-tight w-full font-bold px-4 text-4xl md:text-[64px] lg:text-[90px] text-center mb-20 select-none">
+    {text}
+  </p>
+);
---
+++ src/components/sheet.tsx
@@ -0,0 +1,5 @@
+export const Sheet = ({ children }: { children: React.ReactNode }) => (
+  <main className="container max-w-4xl mx-auto mb-auto space-y-4 md:space-y-10">
+    {children}
+  </main>
+);
