9657fbf720c124d31341177a60555a56464affb0
ceedadev <ceedadev@gmail.com>
2023-11-29 10:28:58 +0700

    work on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,33 @@
+import { SITE_TITLE } from "@/lib/constant";
 import { getAllPostForHome } from "@/lib/api";
-import PostPreviewCard from "@/components/post-preview-card";
+
+import { Sheet } from "@/components/sheet";
+import { PageHeading } from "@/components/page-heading";
+import { CategoryButton } from "@/components/category-button";
+import { PostPreviewGridItem } from "@/components/post-preview-grid-item";
 
 export default async function Home() {
   const data = await getAllPostForHome("");
   return (
-    <main className="container mx-auto mb-auto">
-      <div className="grid gap-4">
+    <Sheet>
+      <PageHeading text={SITE_TITLE} />
+      {/* CATEGORIES */}
+      <div className="flex flex-row items-center justify-between">
+        <p className="uppercase text-xs font-bold tracking-tight">Categories</p>
+        <div className="flex flex-row space-x-2">
+          <CategoryButton category="All" href="/" />
+          <CategoryButton category="Basics" href="/" />
+          <CategoryButton category="Techniques" href="/" />
+          <CategoryButton category="Stories" href="/" />
+          <CategoryButton category="Recipes" href="/" />
+        </div>
+      </div>
+
+      <div className="grid border-separate grid-cols-1 md:grid-cols-3">
         {data.edges.map(({ node }: any) => (
-          <PostPreviewCard key={node.id} node={node} />
+          <PostPreviewGridItem key={node.slug} post={node} />
         ))}
       </div>
-    </main>
+    </Sheet>
   );
 }
