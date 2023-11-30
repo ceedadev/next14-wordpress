78db859f062a3484e87a20dd2ad488a032940bb7
ceedadev <ceedadev@gmail.com>
2023-11-30 19:21:29 +0700

    Update footer and header styles

--- src/components/main-footer.tsx
+++ src/components/main-footer.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function MainFooter() {
   return (
-    <footer className="p-4 md:p-6 border-t mt-10">
-      <div className="container flex flex-row justify-between">
+    <footer className="max-w-4xl mx-auto w-full  border-t mt-10">
+      <div className="container flex flex-row justify-between px-4 md:px-0 py-2">
         <p className="text-xs font-light text-neutral-600">
           made by{" "}
           <Link href={"https://github.com/ceedadev"} className="font-semibold">
--- src/components/main-header.tsx
+++ src/components/main-header.tsx
@@ -6,9 +6,9 @@ export default function MainHeader() {
   return (
     <header className=" max-w-4xl mx-auto w-full border-b mb-4 md:mb-10">
       <div className=" flex flex-row mx-auto px-4 md:px-0 py-2">
-        <p className=" w-full uppercase font-bold tracking-tight">
+        <Link href={"/"} className=" w-full uppercase font-bold tracking-tight">
           {SITE_TITLE}
-        </p>
+        </Link>
 
         <nav className="hidden md:flex flex-row items-center justify-between space-x-4 text-xs uppercase tracking-tight">
           <p>Blog</p>
