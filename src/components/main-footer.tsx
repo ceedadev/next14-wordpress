ea5c8eabc9d07ea63c541a2dcc3b9dc22f6f2216
ceedadev <ceedadev@gmail.com>
2023-12-04 20:32:51 +0700

    added padding

--- src/components/main-footer.tsx
+++ src/components/main-footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function MainFooter() {
   return (
-    <footer className="max-w-4xl mx-auto w-full  border-t mt-10">
+    <footer className="max-w-4xl mx-auto w-full pb-10 border-t mt-10">
       <div className="container flex flex-row justify-between px-4 md:px-0 py-2">
         <p className="text-xs font-light text-neutral-600">
           made by{" "}
